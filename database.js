db0f238e8b2831b50b0d89c8f862135c430d3f26
duongxuannhan2002 <sieuxaya37.na@gmail.com>
2024-06-22 09:33:42 +0700

    vvvvv

--- database.js
+++ database.js
@@ -3,7 +3,7 @@ const pool = mysql.createPool({
     host: 'bxlbmmz5f9s7rjy7e4gx-mysql.services.clever-cloud.com',
     port: 3306,
     user: 'uzoertu66xdfeico',
-    password:'uzoertu66xdfeico',
+    password:'Iqz8DZPMXS0uC1E3mQY5',
     database: 'bxlbmmz5f9s7rjy7e4gx',
     waitForConnections: true,
     connectionLimit: 0,
