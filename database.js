65a73951461a6e34b93cc116f56e30f4d5d6ae93
duongxuannhan2002 <sieuxaya37.na@gmail.com>
2024-06-22 09:22:21 +0700

    vvv

--- database.js
+++ database.js
@@ -1,10 +1,10 @@
 import mysql from 'mysql2'
 const pool = mysql.createPool({
-    host: 'localhost',
-    port: 3310,
-    user: 'root',
-    password:'123456',
-    database: 'thang',
+    host: 'bxlbmmz5f9s7rjy7e4gx-mysql.services.clever-cloud.com',
+    port: 3306,
+    user: 'uzoertu66xdfeico',
+    password:'uzoertu66xdfeico',
+    database: 'bxlbmmz5f9s7rjy7e4gx',
     waitForConnections: true,
     connectionLimit: 0,
     queueLimit: 10,
